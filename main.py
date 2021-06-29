268355fbc6322d387dfff755a6581cb3f7846c30
Trent <53923200+trentstauff@users.noreply.github.com>
2021-06-29 17:38:44 -0400

    cleans up main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import tpqoa
-import yfinance as yf
-import matplotlib.pyplot as plt
 from datetime import datetime
 
 from livetrading.BollingerBandsLive import BollingerBandsLive
@@ -9,18 +7,11 @@ from livetrading.MLClassificationLive import MLClassificationLive
 from livetrading.MomentumLive import MomentumLive
 from livetrading.SMALive import SMALive
 
-from backtesting.IterativeBacktest import IterativeBacktest
 from backtesting.ContrarianBacktest import ContrarianBacktest
 from backtesting.BollingerBandsBacktest import BollingerBandsBacktest
 from backtesting.MomentumBacktest import MomentumBacktest
-from backtesting.MultipleRegressionModelPredictor import MultipleRegressionModelPredictor
 from backtesting.SMABacktest import SMABacktest
 from backtesting.MLClassificationBacktest import MLClassificationBacktest
-
-def check_input(input):
-    if input == "q":
-        return False
-    return True
 
 
 if __name__ == "__main__":
@@ -282,82 +273,3 @@ if __name__ == "__main__":
                 trader.plot_results()
 
                 # TODO: Post trading analysis, maybe some graphs etc
-
-
-        # obj = SMABacktest("EUR_USD", "2010-01-01", "2020-06-30", 30, 200, "D", 0)
-        # obj.test()
-        # obj.plot_results()
-        # print(obj.get_results())
-        # obj.optimize()
-        # obj = SMABacktest("EUR_USD", 30, 200, "2010-01-01", "2020-06-30", "D", 0.1)
-        # print(obj.test())
-        # obj.plot_results()
-        # print(obj.get_results())
-        # print(obj.optimize())
-        # obj.plot_results()
-        # obj.plot_results()
-
-        # obj = ContrarianBacktest("EUR_USD", "2010-01-01", "2020-06-30", 20, "D", 0)
-        # obj.test()
-        # obj.plot_results()
-        # obj.optimize()
-        # obj.plot_results()
-        # print(obj)
-
-        # obj = MomentumBacktest("EUR_USD", "2010-01-01", "2020-06-30", window=1, granularity="D", trading_cost=0)
-        #
-        # obj.test()
-        # obj.plot_results()
-        # obj.optimize()
-        # obj.plot_results()
-        # print(obj)
-
-        # obj = MomentumBacktest("EUR_USD", "2010-01-01", "2020-06-30", 20, "D", 0.0001)
-        # print(obj.optimize())
-        # obj.plot_results()
-
-        # obj = MultipleRegressionModelPredictor("EUR_USD", ("2019-01-01", "2019-12-30"), ("2020-01-01", "2020-08-30"), 5, granularity="M30", trading_cost=0.000015)
-        # obj.test()
-        # obj.plot_results()
-        # obj.get_hitratio()
-
-        # obj = MLClassificationBacktest("EUR_USD", "2019-01-01", "2020-08-30", granularity="M5", trading_cost=0)
-        # print(obj.get_data())
-        # obj.test()
-        #
-        # obj.plot_results()
-        # obj.get_hitratio()
-
-        # obj = IterativeBacktest(
-        #     "oanda.cfg",
-        #     "EUR_USD",
-        #     "2006-12-31",
-        #     "2020-06-30",
-        #     100000,
-        #     granularity="D",
-        #     use_spread=False,
-        # )
-        # obj.test_contrarian(1)
-        # stop = datetime(2021, 6, 21, 21, 4, 6)
-
-
-
-        # api_info()
-
-        # helpers.find_optimal_trading_time("oanda.cfg", "EUR_USD", "2019-07-21", "2020-10-20", granularity="1H")
-        # td = BollingerBandsLive("oanda.cfg", "EUR_USD", "1m", sma=20, deviation=1, units=100000, stop_loss=-5)
-        # print("starting stream")
-        # print(td._data)
-
-        # obj.test_contrarian(window=3)
-        # obj.test_bollinger_bands(50)
-        # obj.print_current_balance(0)
-        # obj.print_current_position_value(0)
-        # obj.print_current_balance(-1)
-        # obj.print_current_position_value(-1)
-        # obj.print_current_nav(-1)
-        # obj.close_position(-1)
-        # print(obj._data)
-        # obj.close_position(-1)
-        # obj.plot_data("spread")
-        # print(obj.bar_info(100))
